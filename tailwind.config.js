632441b94a8865ab2ab46131bfad527aca14c1c7
fahim-sarker <157319749+fahim-sarker@users.noreply.github.com>
2024-10-07 19:35:52 +0600

    first update

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    container: {
+      center: true,
+      width: '1120px',
+    },
+    extend: {
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
